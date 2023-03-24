c555f619c21b76ac48e0f3c7f5eb1d7fb0f3a179
Blond Dev <127840590+BlondHQx@users.noreply.github.com>
2023-03-24 11:02:12 +0100

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function displayJs(){
-    document.querySelector("#node").style.display = "none"
-    document.querySelector("#javascript").style.display = "block"
-}
-
-function displayNode(){
-    document.querySelector("#node").style.display = "block"
-    document.querySelector("#javascript").style.display = "none"
-}
