479ea774a8a4405b686f0c7b48f2690b13893f9b
Blond Dev <127840590+BlondHQx@users.noreply.github.com>
2023-03-24 11:02:29 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function displayJs(){
+    document.querySelector("#node").style.display = "none"
+    document.querySelector("#javascript").style.display = "block"
+}
+
+function displayNode(){
+    document.querySelector("#node").style.display = "block"
+    document.querySelector("#javascript").style.display = "none"
+}
